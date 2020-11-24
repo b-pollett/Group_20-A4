d7e8aeff07e9f7b2a7cd070ea8bfbeb5e04a1619
Bernard Pollett <bpollett18@mun.ca>
2020-11-24 12:26:50 -0330

    Updated team and made division class (no constructor yet)

---
+++ Division.java
@@ -0,0 +1,21 @@
+
+/**
+ * Write a description of class Division here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class Division
+{
+    // instance variables - replace the example below with your own
+    private int x;
+
+    /**
+     * Constructor for objects of class Division
+     */
+    public Division()
+    {
+        // initialise instance variables
+        x = 0;
+    }
+}
---
+++ Team.java
@@ -0,0 +1,25 @@
+
+/**
+ * Write a description of class Team here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class Team
+{
+    // instance variables - replace the example below with your own
+    private String name;
+    private int PLAY;
+    private int COACH;
+   
+    /**
+     * Constructor for objects of class Team
+     */
+    public Team(String name)
+    {
+        // initialise instance variables
+        this.name = name;
+        this.PLAY = 12;
+        this.COACH = 2;
+    }
+}
