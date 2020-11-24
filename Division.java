19565096219b5737a1fd66ebec420176c94c62ac
Bernard Pollett <bpollett18@mun.ca>
2020-11-24 12:22:08 -0330

    Added team constuctor

--- Division.java
+++ Division.java
@@ -5,7 +5,7 @@
  * @author (your name)
  * @version (a version number or a date)
  */
-public class division
+public class Division
 {
     // instance variables - replace the example below with your own
     private int x;
@@ -13,21 +13,9 @@ public class division
     /**
      * Constructor for objects of class divisions
      */
-    public division()
+    public Division()
     {
         // initialise instance variables
         x = 0;
-    }
-
-    /**
-     * An example of a method - replace this comment with your own
-     *
-     * @param  y  a sample parameter for a method
-     * @return    the sum of x and y
-     */
-    public int sampleMethod(int y)
-    {
-        // put your code here
-        return x + y;
     }
 }
