e687a6168a577b0721d53bebf27185b74cf07157
Bernard Pollett <bpollett18@mun.ca>
2020-11-24 19:09:35 -0330

    made addteam to division made division constructor

--- Division.java
+++ Division.java
@@ -1,21 +1,35 @@
-
+import java.util.ArrayList;
 /**
- * Write a description of class Division here.
+ * Division class to hold a set of 5 teams and compare their points 
+ * to provide a standings.
  *
- * @author (your name)
- * @version (a version number or a date)
+ * @Bernard Pollett
+ * @20201124
  */
 public class Division
 {
     // instance variables - replace the example below with your own
     private String name;
-
+    private ArrayList<Team> teams;
+    
     /**
-     * Constructor for objects of class Division
+     * Constructor for objects of class Division, used mainly to give the
+     * Division a name and create array to place teams in.
      */
     public Division(String name)
     {
         // initialise instance variables
         this.name = name;
+        ArrayList<Team> teams = new ArrayList<>();
+    }
+    
+    /**
+     * Constructor for objects of class Division, used mainly to give the
+     * Division a name and create array to place teams in.
+     */
+    public void addTeam(Team team)
+    {
+        if (teams.size() != 5){teams.add(team);}
+        else {throw new java.lang.Error("Division is full");}
     }
 }
--- Team.java
+++ Team.java
@@ -1,13 +1,14 @@
 
 /**
- * Write a description of class Team here.
+ * Team class, used to hold name of team and different points
+ * from matches.
  *
- * @author (your name)
- * @version (a version number or a date)
+ * @Bernard Pollett
+ * @v20201124
  */
 public class Team
 {
-    // instance variables - replace the example below with your own
+    // instance variables
     private String name;
     private int PLAY;
     private int COACH;
@@ -16,6 +17,7 @@ public class Team
    
     /**
      * Constructor for objects of class Team
+     * sets name, players, coaches, set_pts, and pts_diff.
      */
     public Team(String name)
     {
@@ -25,5 +27,39 @@ public class Team
         this.COACH = 2;
         this.set_pts = 0;
         this.pts_diff = 0;
+    }
+    
+    /**
+     * Method to pull the teams name
+     */
+    public String getName()
+    {
+        return name;
+    }
+    
+    /**
+     * Method to pull the teams points from matches and sets.
+     */
+    public int getPts()
+    {
+        return set_pts;
+    }
+    
+    /**
+     * Method to pull the teams total points difference from games.
+     */
+    public int getDiff()
+    {
+        return pts_diff;
+    }
+    
+    /**
+     * Method to pull the teams name
+     */
+    public String ShowInfo()
+    {
+        return "Team Name: " + name + 
+        "\nNumber of Players: " + PLAY +
+        "\nNumber of Coaches: " + COACH;
     }
 }
